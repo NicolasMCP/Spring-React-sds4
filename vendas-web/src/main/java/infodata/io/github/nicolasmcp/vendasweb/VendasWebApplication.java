5bc0afa4db6598e39d8ba3f27044f4e9fb16f303
NicolasMCP <nicolas.mcp@gmail.com>
2021-09-07 23:26:24 -0300

    Backend criado

---
+++ vendas-web/src/main/java/infodata/io/github/nicolasmcp/vendasweb/VendasWebApplication.java
@@ -0,0 +1,13 @@
+package infodata.io.github.nicolasmcp.vendasweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasWebApplication.class, args);
+	}
+
+}
---
+++ vendas-web/src/test/java/infodata/io/github/nicolasmcp/vendasweb/VendasWebApplicationTests.java
@@ -0,0 +1,13 @@
+package infodata.io.github.nicolasmcp.vendasweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
